056ddf706e89e9e515f572279a20be71056042d1
Trikita73 <diachenkoandrii7@gmail.com>
2022-12-17 18:36:49 +0200

    normal_scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// Parallax -->
 window.addEventListener('scroll', e => {
-    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+
+// Scroll_gsap -->
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content'
 })
